6a13bf376aea34d6351b4987a9d5ae81b1970be6
tomaszkoczar <tomekkoczar@gmail.com>
2025-01-26 17:24:29 +0100

    local host

--- frontend/src/api/exercises.js
+++ frontend/src/api/exercises.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api/exercises';
+const API_URL = 'http://127.0.0.1:5001/api/exercises';
 
 export const fetchExercises = () => axios.get(API_URL);
 export const createExercise = (exercise) => axios.post(API_URL, exercise);
